f822d731ff0aa1c3360ee75cb1489e0514f1ba91
HANEKUN <himono0722@gmail.com>
2021-02-09 16:29:18 +0900

    2章

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-alert('Hello World');
+var hello = 'Hello World';
+alert(hello);
+
+
+// 足し算
+alert('4足す3は' + (4 + 3));
+
+// 引き算
+alert('8引く5は' + (8 - 5));
+
+// 掛け算
+alert('2掛ける6は' + (2 * 6));
+
+// 割り算
+alert('10割る2は'  + (10 / 2));
